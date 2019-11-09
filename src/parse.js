630dc9f44d90885b7ca4396e24ac6b599fd5c19a
Hugo Josefson <hugo@josefson.org>
2019-11-09 12:54:31 +0000

    Simplify.

--- src/parse.js
+++ src/parse.js
@@ -1,16 +1,17 @@
-import parse2LinesOfHints from './parse-2-lines-of-hints'
+import parse1LineOfHints from './parse-1-line-of-hints'
 
 export default input => {
   if (typeof input !== 'string') {
     throw new Error('Input must be a string.')
   }
 
-  const lines = input.trim().split('\n')
+  const linesOfHints = input.trim().split('\n')
     .map(line => line.replace(/#.*/g, ''))
     .map(line => line.trim())
     .filter(line => line.length)
-  if (lines.length === 2) {
-    return parse2LinesOfHints(lines)
+
+  if (linesOfHints.length === 2) {
+    return linesOfHints.map(parse1LineOfHints)
   }
 
   throw new Error('Input must be 2 lines of hints.')
